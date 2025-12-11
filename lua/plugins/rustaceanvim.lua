5d8b82bd711e4cdbbdda3229575a4d75bda80f74
lastsunday <lastsunday@yeah.net>
2025-12-11 15:29:29 +0800

    fix: rust async-trait warning

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,56 +1,13 @@
 return {
   "mrcjkb/rustaceanvim",
-  version = "^6", -- Recommended
-  lazy = false, -- This plugin is already lazy
   opts = {
     server = {
-      on_attach = function(_, bufnr)
-        vim.keymap.set("n", "<leader>cR", function()
-          vim.cmd.RustLsp("codeAction")
-        end, { desc = "Code Action", buffer = bufnr })
-        vim.keymap.set("n", "<leader>dr", function()
-          vim.cmd.RustLsp("debuggables")
-        end, { desc = "Rust Debuggables", buffer = bufnr })
-      end,
       default_settings = {
-        -- rust-analyzer language server configuration
         ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = false,
-            loadOutDirsFromCheck = true,
-            buildScripts = {
-              enable = true,
-            },
-          },
-          runnables = {
-            -- extraTestBinaryArgs = {},
-          },
-          -- Add clippy lints for Rust if using rust-analyzer
-          checkOnSave = true,
-          -- Enable diagnostics if using rust-analyzer
-          diagnostics = {
-            enable = true,
-            disabled = { "unresolved-proc-macro" },
-          },
           procMacro = {
-            enable = true,
             ignored = {
-              ["async-trait"] = { "async_trait" },
-              ["napi-derive"] = { "napi" },
-              ["async-recursion"] = { "async_recursion" },
-            },
-          },
-          files = {
-            excludeDirs = {
-              ".direnv",
-              ".git",
-              ".github",
-              ".gitlab",
-              "bin",
-              "node_modules",
-              "target",
-              "venv",
-              ".venv",
+              -- https://github.com/LazyVim/LazyVim/discussions/5638
+              ["async-trait"] = vim.NIL,
             },
           },
         },
